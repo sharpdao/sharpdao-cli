18ee545adfe6c6799b63d8847acc9f0b42335edb
Kyle Johns <kjohns1337@gmail.com>
2022-10-15 22:32:26 -0400

    better definition of the Treasury

--- SharpDao.Cli/Models/Treasury.cs
+++ SharpDao.Cli/Models/Treasury.cs
@@ -5,11 +5,12 @@ namespace SharpDao.Cli.Models;
 public class Treasury: MonetaryAmount
 {
     public OneOrMany<Dao> Dao { get; set; } //DAO DID Ref
-    
-    //currency = symbol of the Token
-    //name = the name of the Token
-    //value = how many to mint now
-    //maxValue = max supply
-    //validThrough = only if we know were not minting more
+    public OneOrMany<Person> Manager { get; set; } //Person(s) who can authorize transfer of funds
+    public OneOrMany<string> TokenName { get; set; } //name of the Token
+    public OneOrMany<string> TokenSymbol { get; set; } //symbol of the Token
+
+    //value = how many Tokens are in the Treasury
+    //maxValue = max supply, optional
+    //validThrough = last datetime we can mint more Tokens, optional
     //validFrom = the created date of the DAO
 }
